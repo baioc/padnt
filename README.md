28c72c93c8e7ed09b6f1a92165bb2a19e5b2ba18
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2023-02-12 13:20:18 -0300

    Test H2 access and logging

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## Refs
 
-* [How to log](https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/html/howto.html#howto.logging)
-* [Logging features](https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/html/features.html#features.logging)
-* [How to access databases](https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/html/howto.html#howto.data-access)
+* [DDD / Hexagonal Architecture](https://www.baeldung.com/hexagonal-architecture-ddd-spring)
+* JPA + H2 - [How to access databases](https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/html/howto.html#howto.data-access)
 * [Spring Boot Actuator](https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/html/actuator.html#actuator)
 
 ## Guides
--- src/main/java/baioc/dontpad/Controller.java
+++ src/main/java/baioc/dontpad/Controller.java
@@ -2,13 +2,27 @@ package baioc.dontpad;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @RestController
 public class Controller {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@GetMapping("/")
 	public String index() {
 		return "Hello, Spring World!";
+	}
+
+	@GetMapping("/log")
+	public String testLog() {
+		logger.error("this is a error-level log message");
+		logger.warn("this is a warn-level log message");
+		logger.info("this is a info-level log message");
+		logger.debug("this is a debug-level log message");
+		logger.trace("this is a trace-level log message");
+		return "something was logged";
 	}
 
 }
